e4eb475e7c76666070886b0ccf0fb1bd959d3b9c
mrawal <imsr1013@gmail.com>
2022-07-05 17:08:20 +0545

    Setup account functionality to our website and setup basic html template files for homepage

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.core.urlreslovers import reverse_lazy
+from django.views.generic import CreateView
+
+from . import forms
+
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
+
